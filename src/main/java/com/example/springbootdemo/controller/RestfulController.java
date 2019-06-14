e2f2e0471b8f06727103d3307092fb9736020f20
lgy <974942485@qq.com>
2019-06-14 16:50:41 +0800

    逆向工程

--- src/main/java/com/example/springbootdemo/controller/RestfulController.java
+++ src/main/java/com/example/springbootdemo/controller/RestfulController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+
 @RestController
 public class RestfulController {
 
---
+++ src/main/java/com/example/springbootdemo/dao/UserMapper.java
@@ -0,0 +1,27 @@
+package com.example.springbootdemo.dao;
+
+import com.example.springbootdemo.entity.User;
+import com.example.springbootdemo.entity.UserExample;
+import com.example.springbootdemo.entity.UserWithBLOBs;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    int countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int insert(UserWithBLOBs record);
+
+    int insertSelective(UserWithBLOBs record);
+
+    List<UserWithBLOBs> selectByExampleWithBLOBs(UserExample example);
+
+    List<User> selectByExample(UserExample example);
+
+    int updateByExampleSelective(@Param("record") UserWithBLOBs record, @Param("example") UserExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") UserWithBLOBs record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+}
