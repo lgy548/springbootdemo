956e7a316cafaf2cfd7e197aab69e94fbc35afb3
lgy <974942485@qq.com>
2019-07-25 17:39:02 +0800

    添加页面vue

--- src/main/java/com/example/springbootdemo/controller/RestfulController.java
+++ src/main/java/com/example/springbootdemo/controller/RestfulController.java
@@ -1,20 +1,33 @@
 package com.example.springbootdemo.controller;
 
 import com.example.springbootdemo.mapper.UserMapper;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 
-@RestController
+@Controller
 public class RestfulController {
 
     @Resource
     private UserMapper userMapper;
 
-    @PostMapping("/select")
-    public int postApi(){
-        return userMapper.selectId();
+    @RequestMapping("/test")
+    public String  index(){
+        return "index";
     }
+
+    @ResponseBody
+    @RequestMapping("/select")
+    public Map<String,Object> select(){
+        int i = userMapper.selectId();
+        Map<String,Object> map = new HashMap<>();
+        map.put("id",i);
+        return map;
+    }
+
 }
